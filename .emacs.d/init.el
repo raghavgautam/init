db84adacbc3175ab442da9301ba6f321b38155ac
Raghav Kumar Gautam <raghavgautam@gmail.com>
2014-02-21 15:28:22 -0800

    Setting some modes according to file extention.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,2 +1,4 @@
 (ido-mode 1)
 (put 'narrow-to-region 'disabled nil)
+(setq auto-mode-alist (cons '("\\.txt$" . auto-revert-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.log$" . auto-revert-tail-mode) auto-mode-alist))
