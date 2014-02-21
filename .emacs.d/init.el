b25be683dd74e1b89bae957645b3ce8eb7ec743f
Raghav Kumar Gautam <raghavgautam@gmail.com>
2014-02-21 15:33:07 -0800

    Always dired with autorevert.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,3 +2,4 @@
 (put 'narrow-to-region 'disabled nil)
 (setq auto-mode-alist (cons '("\\.txt$" . auto-revert-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\.log$" . auto-revert-tail-mode) auto-mode-alist))
+(setq dired-auto-revert-buffer t)
