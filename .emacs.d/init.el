ae50f58d55a3d30f9497f63b1d072d70c75e7d90
Raghav Kumar Gautam <raghavgautam@gmail.com>
2014-04-05 11:43:56 -0700

    enabling ido everwhere

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,5 @@
+(setq ido-enable-flex-matching t
+      ido-everywhere t)
 (ido-mode 1)
 (put 'narrow-to-region 'disabled nil)
 (setq auto-mode-alist (cons '("\\.txt$" . auto-revert-mode) auto-mode-alist))
