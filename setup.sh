2052405a0b22bc63e846d0ba88e463b9348cbe2d
Raghav Kumar Gautam <raghavgautam@gmail.com>
2014-05-07 15:27:25 -0700

    adding setup script

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+BASEDIR=$(dirname $0)
+PUB_KEY_FILE=$BASEDIR/*.pub
+SCREENRC_FILE=$BASEDIR/.screenrc
+ME=`whoami'
+
+cat PUB_KEY_FILE >> ~/.ssh/authorized_keys*
+mv $SCREENRC_FILE ~/.screenrc
+
+if [ $ME="root" ]; then
+yum -f install emacs screen tree git
+else
+
+fi
