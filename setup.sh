dcf6bb157d3e4b264bc421aeb58e767e5f5887c8
Raghav Kumar Gautam <raghavgautam@gmail.com>
2014-05-07 15:35:23 -0700

    minor fix

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ PUB_KEY_FILE=$BASEDIR/*.pub
 SCREENRC_FILE=$BASEDIR/.screenrc
 ME=`whoami`
 
-cat PUB_KEY_FILE >> ~/.ssh/authorized_keys*
+cat $PUB_KEY_FILE >> ~/.ssh/authorized_keys*
 mv $SCREENRC_FILE ~/.screenrc
 
 if [ $ME="root" ]; then
