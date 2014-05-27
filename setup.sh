0865ab650b4113d19caadc70dcd2649bccc82f80
Raghav Kumar Gautam <raghavgautam@gmail.com>
2014-05-27 15:54:45 -0700

    assume files to be present in .emacs.d dir

--- setup.sh
+++ setup.sh
@@ -14,15 +14,17 @@ if [ ! -f $ME_FILE ]; then
     touch $ME_FILE
 fi
 
-rm -rf ~/.emacs.d || true
+#rm -rf ~/.emacs.d || true
 rm -rf ~/.emacs || true
-cp -R $EMACS_INIT ~/.emacs.d
+#cp -R $EMACS_INIT ~/.emacs.d
 
 if [ "$ME" == "root" ]; then
     if hash yum 2>/dev/null; then
 	yum -y install $PKGS
     elif hash apt-get 2>/dev/null; then
 	apt-get -y install $PKGS
+    elif hash zypper 2>/dev/null; then
+	zypper -y install $PKGS
     else
 	echo "unknown package manager"
     fi 
