492d3635dac258389b5cb6f04477de5046181a24
Raghav Kumar Gautam <raghavgautam@gmail.com>
2014-07-17 12:39:31 -0700

    remove ~/.emacs.d/lisp dir

--- lisp/fr-mode.el
+++ lisp/fr-mode.el
@@ -69,7 +69,7 @@
 (defun fr-set-vars (host)
   (interactive
    (list (read-from-minibuffer "Hostname: " (gethostname))))
-  (setq setup-cmd "curl -L https://github.com/raghavgautam/init/archive/master.zip -o /tmp/`whoami`.zip; unzip -o -d /tmp/`whoami`/ /tmp/`whoami`.zip; chmod -R 777 /tmp/`whoami`/; mkdir ~/.emacs.d/; mv /tmp/`whoami`/init-master/* /tmp/`whoami`/init-master/.[^.]* ~/.emacs.d/; ~/.emacs.d/setup.sh")
+  (setq setup-cmd "curl -L https://github.com/raghavgautam/init/archive/master.zip -o /tmp/`whoami`.zip; unzip -o -d /tmp/`whoami`/ /tmp/`whoami`.zip; chmod -R 777 /tmp/`whoami`/; mkdir ~/.emacs.d/; rm -rf ~/.emacs.d/lisp; mv /tmp/`whoami`/init-master/* /tmp/`whoami`/init-master/.[^.]* ~/.emacs.d/; ~/.emacs.d/setup.sh")
   (setq host-name host
 	ooz-bin "oozie"
 	ooz-url (concat "http://" host-name ":11000/oozie")
