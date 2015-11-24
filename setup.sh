1c5b07f86376c174f6b5963657950d3f2f436c32
Raghav Kumar Gautam <raghav@apache.org>
2015-11-23 20:44:01 -0800

    moving installation to dotfiles repository

--- setup.sh
+++ setup.sh
@@ -6,7 +6,6 @@ SCREENRC_FILE=$BASEDIR/.screenrc
 EMACS_INIT=$BASEDIR/.emacs.d
 ME=`whoami`
 ME_FILE=$BASEDIR/$ME
-PKGS="emacs screen tree git tmux"
 
 if [ ! -f $ME_FILE ]; then
     cp -f $SCREENRC_FILE ~/.screenrc
@@ -17,20 +16,3 @@ fi
 #rm -rf ~/.emacs.d || true
 rm -rf ~/.emacs || true
 #cp -R $EMACS_INIT ~/.emacs.d
-
-if [ "$ME" == "root" ]; then
-    if hash yum 2>/dev/null; then
-	yum -y install $PKGS
-    elif hash apt-get 2>/dev/null; then
-	apt-get -y install $PKGS
-    elif hash zypper 2>/dev/null; then
-	if [ -f "/etc/SuSE-release" ]; then
-	    zypper addrepo -n -f http://download.opensuse.org/distribution/11.4/repo/oss/ oss
-	fi
-	zypper -n install $PKGS
-    else
-	echo "unknown package manager"
-    fi 
-else
-    echo "not installing anything"
-fi
