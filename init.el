352f1e38e0a79155560a0fe3d0ffeff4ed2d283e
Raghav Kumar Gautam <raghav@apache.org>
2015-12-15 15:43:21 -0800

    faster startup

--- init.el
+++ init.el
@@ -25,30 +25,34 @@
 
 
 (setq url-proxy-services '(("no_proxy" . ".*")))
-(when (equal emacs-major-version 23)
-  (if (load (expand-file-name "~/.emacs.d/elpa/package.el") t)
-      (package-initialize)
-    (let ((buffer
-	   (url-retrieve-synchronously "http://git.savannah.gnu.org/gitweb/?p=emacs.git;a=blob_plain;hb=ba08b24186711eaeb3748f3d1f23e2c2d9ed0d09;f=lisp/emacs-lisp/package.el")))
-      (if (not buffer)
-	  (message "Attempt to fetch package-install.el failed.")
-	(with-current-buffer buffer
-	  (goto-char (point-min))
-	  (re-search-forward "^$" nil 'move)
-	  (eval-region (point) (point-max))
-	  (kill-buffer (current-buffer)))))))
+(let ((pkg-file (expand-file-name "~/.emacs.d/elpa/package.el")))
+  (when (equal emacs-major-version 23)
+    (if (load pkg-file t)
+	(package-initialize)
+      (let ((buffer
+	     (url-retrieve-synchronously "http://git.savannah.gnu.org/gitweb/?p=emacs.git;a=blob_plain;hb=ba08b24186711eaeb3748f3d1f23e2c2d9ed0d09;f=lisp/emacs-lisp/package.el")))
+	(if (not buffer)
+	    (message "Attempt to fetch package-install.el failed.")
+	  (with-current-buffer buffer
+	    (goto-char (point-min))
+	    (re-search-forward "^$" nil 'move)
+	    (delete-region (point-min) (point))
+	    (eval-region (point) (point-max))
+	    (write-file pkg-file)
+	    (kill-buffer (current-buffer))))))))
 
 (when (and (>= emacs-major-version 23) (not (bound-and-true-p laptop))  (require 'package nil 'noerror))
   (message "loading package manager stuff")
   (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
   (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-  (ignore-errors
-    (package-refresh-contents)
-    (package-install 'tramp-hdfs)
-    (package-install 'osx-lib)
-    (package-install 'helm)
-    (package-install 'helm-jstack)
-    (package-install 'helm-wordnet)))
+  (unless (ignore-errors (require 'tramp-hdfs))
+    (ignore-errors
+      (package-refresh-contents)
+      (package-install 'tramp-hdfs)
+      (package-install 'osx-lib)
+      (package-install 'helm)
+      (package-install 'helm-jstack)
+      (package-install 'helm-wordnet))))
 
 (add-hook 'fr-mode-hook 'auto-revert-mode)
 (setq auto-mode-alist (cons '("\\.log$" . auto-revert-tail-mode) auto-mode-alist))
